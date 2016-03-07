ce1b2ebb8f5ea75560432ddbb4d39e8224e6df72
jinghewang <jinghewang@163.com>
2016-03-07 15:05:53 +0800

    test commit

--- index.php
+++ index.php
@@ -12,5 +12,57 @@ require_once("test/Mammal.php");
 require_once("test/Person.php");
 
 
-$person =new \test\Person('wjh',35);
-$person->testName();
+$person = new \test\Person('wjh', 35);
+//$person->testName3(['age'=>'232']);
+
+
+
+function barber($type)
+{
+    echo "You wanted a $type haircut, no problem\n";
+}
+call_user_func('barber', "mushroom");
+call_user_func('barber', "shave");
+
+die;
+
+
+
+\test\Person::hello();
+
+/**
+ * @param $person
+ */
+function testEcho($person)
+{
+//echo print
+    echo '222', $person->testName();
+    print '333' . $person->testName();
+}
+
+//testEcho($person);
+
+function testImage()
+{
+    //imagesize
+    $file = __DIR__ . '/mm.jpg';
+    $imageSize = getimagesize($file);
+    print_r($imageSize);
+}
+
+//testImage();
+
+
+//define('cmp','hlt');
+//echo cmp;
+
+error_reporting(E_ALL);
+function increment(&$var)
+{
+    var_dump('----1111----');
+    $var++;
+}
+
+$a = 0;
+call_user_func(increment, $a);
+echo $a."\n";
--- test/Animal.php
+++ test/Animal.php
@@ -61,11 +61,42 @@ class Animal
 
     public function testName()
     {
-        echo $this->name;
+        return $this->name;
     }
 
     public static function testName2(){
         echo 'testName2';
+    }
+
+    function __call($name, $arguments)
+    {
+        echo "<b>function name:</b>";
+        echo $name . '<br>';
+
+        echo "<b>function arguments:</b>";
+        print_r( $arguments );
+    }
+
+
+
+    public static function helloCall($name,$age){
+        var_dump($name,$age);
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        echo "<b>function name2:</b>";
+        echo $name . '<br>';
+
+        echo "<b>function arguments2:</b>";
+        print_r( $arguments );
+
+        if ($name == 'hello'){
+            var_dump('----3333333333----');
+            //call_user_func(function($arr){ var_dump($arr);},[123,456]);
+
+            //call_user_func($this->helloCall(),[123,456]);
+        }
     }
 
 
